ad888816320006b76b13e6457847715cc5656b74
sativaatha <sativa_azzahra_33rpl@student.smktelkom-mlg.sch.id>
2024-10-14 14:49:41 +0700

    Add files via upload

---
+++ TesterCD.java
@@ -0,0 +1,17 @@
+package Toko;
+
+public class TesterCD {
+    public static void main(String[] args) {
+        CD a = new CD();
+
+       a.setNumber(2);
+       a.setName("Rebron");
+       a.setQuantity(12);
+       a.setPrice(2000.00);
+       a.setArtist("Jay");
+       a.setNumSong(10);
+       a.setLabel("LG");
+       a.print();
+    }
+    
+}
